876a37db1c17b8bee2489706ad78f2eae66ba7f2
oguyhu <oalpcd@gmail.com>
2020-06-19 18:47:38 +0900

    third commit

--- app/services/UserServiceImpl.scala
+++ app/services/UserServiceImpl.scala
@@ -1,9 +1,9 @@
 package services
 
 import javax.inject.Singleton
-
-import models.User
-import scalikejdbc.{ AutoSession, DBSession }
+import models.{PagedItems, User}
+import scalikejdbc.{AutoSession, DBSession}
+import skinny.Pagination
 
 import scala.util.Try
 
@@ -20,4 +20,15 @@ class UserServiceImpl extends UserService {
       User.where('email -> email).apply().headOption
     }
 
+  def findAll(pagination: Pagination)(implicit dbSession: DBSession = AutoSession): Try[PagedItems[User]] = Try {
+    PagedItems[User](
+      pagination,
+      User.countAllModels(),
+      User.findAllWithPagination(pagination, Seq(User.defaultAlias.id.asc))
+    )
+  }
+
+  override def findById(id: Long)(implicit dbSession: DBSession): Try[Option[User]] = Try {
+    User.findById(id)
+  }
 }
---
+++ conf/db/migration/default/mysql/V2__Add_Users.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test1', 'test1@test.com', '$2a$10$TDLdis6Cxp2P90WN2etmRu5paFUi/vxgx7xZGpZarOaK2jbi1jsYS', now(), now());
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test2', 'test2@test.com', '$2a$10$TDLdis6Cxp2P90WN2etmRu5paFUi/vxgx7xZGpZarOaK2jbi1jsYS', now(), now());
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test3', 'test3@test.com', '$2a$10$TDLdis6Cxp2P90WN2etmRu5paFUi/vxgx7xZGpZarOaK2jbi1jsYS', now(), now());
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test4', 'test4@test.com', '$2a$10$TDLdis6Cxp2P90WN2etmRu5paFUi/vxgx7xZGpZarOaK2jbi1jsYS', now(), now());
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test5', 'test5@test.com', '$2a$10$TDLdis6Cxp2P90WN2etmRu5paFUi/vxgx7xZGpZarOaK2jbi1jsYS', now(), now());
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test6', 'test6@test.com', '$2a$10$TDLdis6Cxp2P90WN2etmRu5paFUi/vxgx7xZGpZarOaK2jbi1jsYS', now(), now());
