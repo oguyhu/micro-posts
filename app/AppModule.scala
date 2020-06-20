1b3be887aa8f5ba83972cc71289eb27df04efd89
oguyhu <oalpcd@gmail.com>
2020-06-20 09:18:16 +0900

    add post

--- app/AppModule.scala
+++ app/AppModule.scala
@@ -1,10 +1,11 @@
 import com.google.inject.AbstractModule
-import services.{UserService, UserServiceImpl}
+import services.{MicroPostService, MicroPostServiceImpl, UserService, UserServiceImpl}
 
 class AppModule extends AbstractModule {
 
   override def configure(): Unit = {
     bind(classOf[UserService]).to(classOf[UserServiceImpl])
+    bind(classOf[MicroPostService]).to(classOf[MicroPostServiceImpl])
   }
 
 }
